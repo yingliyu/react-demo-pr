c70a78727acf2b12f67130c4b32837e9adb33b45
unknown <ylyu@Heyi.com>
2021-11-19 15:25:52 +0800

    test husky

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,8 @@ import { BookOutlined, LinkOutlined } from '@ant-design/icons';
 
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
-
+const a = 1;
+a = 2;
 /** 获取用户信息比较慢的时候会展示一个 loading */
 export const initialStateConfig = {
   loading: <PageLoading />,
